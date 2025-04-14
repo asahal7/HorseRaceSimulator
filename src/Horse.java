c595e958730923c2fbda89f0afb0c3b0125f2d1f
Abdimaalik Sahal <abdimaaaalik0@gmail.com>
2025-04-14 13:41:38 +0100

    Defined five private fields for the horse class, implemented the horse constructor and have added method to hande the horse falling during the race

--- src/Horse.java
+++ src/Horse.java
@@ -1,22 +1,35 @@
 
 /**
- * Write a description of class Horse here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This code represents a horse participiating in a horse race.
+ * Each and every horse has a name , symbol, confidence rating from 0-1 and is able to track its race progress
+ * @author (Abdimaalik Sahal) 
+ * @version (12/04/2025)
  */
 public class Horse
 {
-    //Fields of class Horse
-    
-    
-      
-    //Constructor of class Horse
+    // The five required fields for the horse class are stated below
+    private String name; // stores the name of the horse
+    private char symbol; // the unicode character symbol to represent each horse
+    private int distanceTravelled; // an integer variable which stores how much distance the horse has travelled
+    private boolean hasFallen; // a boolean variable which states true if the horse has fallen and false if not
+    private double confidence; // a double variable which represents how confident the horse is
+
     /**
-     * Constructor for objects of class Horse
-     */
+    * Constructor of class Horse
+    * @param horseSymbol The character used to represent the horse
+    * @param horseName The name of the horse
+    * @param horseConfidence The confidence rating of the horse on a scale of 0 to 1
+    */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        // valid and set the confidence(ensure that the argument lies between 0 and 1);
+        setConfidence(horseConfidence);
+
+        this.distanceTravelled = 0; 
+        this.hasFallen = false;
+
        
     }
     
@@ -25,7 +38,7 @@ public class Horse
     //Other methods of class Horse
     public void fall()
     {
-        
+        this.hasFallen = true;
     }
     
     public double getConfidence()
